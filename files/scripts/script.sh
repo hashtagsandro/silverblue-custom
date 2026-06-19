d6da3a1334c499a64146a79044c62afe3cbd41eb
hashtagsandro <sandrofabbro@gmail.com>
2026-06-19 10:46:58 +0200

    Enable podman start

--- files/scripts/script.sh
+++ files/scripts/script.sh
@@ -6,5 +6,5 @@
 set -oue pipefail
 
 # Your code goes here.
-echo 'This is an example shell script'
-echo 'Scripts here will run during build if specified in recipe.yml'
+echo 'Enable docker service'
+systemctl enable docker
