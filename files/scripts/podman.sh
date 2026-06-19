ad2b148d246c706cd3f650151d267ebf662453bf
hashtagsandro <sandrofabbro@gmail.com>
2026-06-19 11:09:29 +0200

    Rewrite Podman

--- files/scripts/podman.sh
+++ files/scripts/podman.sh
@@ -3,8 +3,11 @@
 # Tell this script to exit if there are any errors.
 # You should have this in every custom script, to ensure that your completed
 # builds actually ran successfully without any errors!
-set -oue pipefail
+set -euo pipefail
 
 # Your code goes here.
 echo 'Enable docker service'
-ln -s /etc/systemd/system/default.target.wants/podman.service /usr/lib/systemd/system/podman.service
+if [ ! -e /usr/lib/systemd/system/podman.service ]; then
+    ln -s /etc/systemd/system/default.target.wants/podman.service \
+          /usr/lib/systemd/system/podman.service
+fi
