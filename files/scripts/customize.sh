21b323665294660a681afce854b5c2eccf8a7673
hashtagsandro <sandrofabbro@gmail.com>
2026-06-19 14:58:35 +0200

    Edit f5cli path

--- files/scripts/customize.sh
+++ files/scripts/customize.sh
@@ -5,3 +5,6 @@ set -euo pipefail
 echo 'Install F5 VPN Client'
 rpm -Uvh --nodigest --nofiledigest /tmp/linux_f5cli.x86_64.rpm
 rm /tmp/linux_f5cli.x86_64.rpm
+
+mv /usr/local/lib/F5Networks /usr/lib/F5Networks
+ln -sf /usr/lib/F5Networks/f5fpc_x86_64 /usr/bin/f5fpc
