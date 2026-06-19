56d698098de046f105f2cb06cfc64b9cf63abefe
hashtagsandro <sandrofabbro@gmail.com>
2026-06-19 15:53:28 +0200

    Add archive support

--- files/scripts/customize.sh
+++ files/scripts/customize.sh
@@ -2,9 +2,11 @@
 
 set -euo pipefail
 
-echo 'Install F5 VPN Client'
-rpm -Uvh --nodigest --nofiledigest /tmp/linux_f5cli.x86_64.rpm
-rm /tmp/linux_f5cli.x86_64.rpm
+echo 'Start Customize Script'
 
-mv /usr/local/lib/F5Networks /usr/lib/F5Networks
-ln -sf /usr/lib/F5Networks/f5fpc_x86_64 /usr/bin/f5fpc
+#echo 'Install F5 VPN Client'
+#rpm -Uvh --nodigest --nofiledigest /tmp/linux_f5cli.x86_64.rpm
+#rm /tmp/linux_f5cli.x86_64.rpm
+#
+#mv /usr/local/lib/F5Networks /usr/lib/F5Networks
+#ln -sf /usr/lib/F5Networks/f5fpc_x86_64 /usr/bin/f5fpc
